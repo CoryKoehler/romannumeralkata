d74b7f3d9f1fbe5f9dcca797363a24fba3acccf1
Cory Koehler <cjiek9181@gmail.com>
2017-07-17 12:14:47 -0400

    forgot to add conversion handler test

---
+++ RomanNumeralsKataTests/ConversionHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using NUnit.Framework;
+using RomanNumeralsKata;
+
+namespace RomanNumeralsKataTests
+{
+    [TestFixture]
+    public class ConversionHandlerTests
+    {
+        private ConversionHandler _conversionHandler;
+        private Mock<IArabicToRomanConverter> _arabicToRomanConverter;
+        private Mock<IRomanToArabicConverter> _romanToArabicConverter;
+
+        [SetUp]
+        public void Setup()
+        {
+            _arabicToRomanConverter = new Mock<IArabicToRomanConverter>();
+            _arabicToRomanConverter.Setup(_ => _.ArabicToRoman(4));
+            _romanToArabicConverter = new Mock<IRomanToArabicConverter>();
+            _romanToArabicConverter.Setup(_ => _.RomanToArabic("IV"));
+
+            _conversionHandler = new ConversionHandler(_arabicToRomanConverter.Object, _romanToArabicConverter.Object);
+        }
+
+        [Test]
+        public void CanConvertRomanNumeralToArabicNumber()
+        {
+            _conversionHandler.HandleArabicToRomanConversion(4);
+            _arabicToRomanConverter.Verify(_ => _.ArabicToRoman(4), Times.AtLeastOnce);
+        }
+
+        [Test]
+        public void CanConvertArabicNumberToRomanNumeral()
+        {
+            _conversionHandler.HandleRomanToArabicConversion("IV");
+            _romanToArabicConverter.Verify(_ => _.RomanToArabic("IV"), Times.AtLeastOnce);
+        }
+    }
+}
